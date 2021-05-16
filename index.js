aedbd620094768dfbf87654a500ebc8ef86a7859
Martin <martbee88@gmail.com>
2021-05-16 12:03:25 -0500

    feat: add espn rss feed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const getSSMSecret = require('./lib/getSSMSecret');
 const getNytArticles = require('./lib/getNytArticles');
 const getBbcArticles = require('./lib/getBbcArticles');
 const getCnnArticles = require('./lib/getCnnArticles');
+const getEspnArticles = require('./lib/getEspnArticles');
 const standardizeArticle = require('./lib/standardizeArticle');
 
 exports.handler = async () => {
@@ -11,6 +12,7 @@ exports.handler = async () => {
     await getNytArticles(nytAPIKey),
     await getBbcArticles(),
     await getCnnArticles(),
+    await getEspnArticles(),
   ]);
 
   const articles = articleGroups.reduce((allArticles, group) => {
