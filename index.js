ba6a729e1ccebba4ab7a02a64892292a227eb050
Martin <martbee88@gmail.com>
2021-05-16 10:40:51 -0500

    feat: refactor and promise.all correctly

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const getSSMSecret = require('./lib/getSSMSecret');
-const setUpNYTApi = require('./lib/setUpNYTApi');
-const setUpBBCApi = require('./lib/setUpBBCApi');
-const setUpCNNApi = require('./lib/setUpCNNApi');
+const getNytArticles = require('./lib/getNytArticles');
+const getBbcArticles = require('./lib/getBbcArticles');
+const getCnnArticles = require('./lib/getCnnArticles');
 
 exports.handler = async () => {
   const nytAPIKey = await getSSMSecret('nyt-key');
-  const nytAPI = setUpNYTApi(nytAPIKey);
-  const bbcAPI = setUpBBCApi();
-  const cnnAPI = setUpCNNApi();
 
-  const stories = await Promise.all(
-    await nytAPI.getTopStories(),
-    await bbcAPI.getStories(),
-    await cnnAPI.getStories()
+  const articleGroups = await Promise.all([
+    await getNytArticles(nytAPIKey),
+    await getBbcArticles(),
+    await getCnnArticles(),
+  ]);
+
+  articleGroups.forEach((group) =>
+    group.forEach((article) => console.log(article.source))
   );
-  console.log(stories);
 };
 
 exports.handler();
